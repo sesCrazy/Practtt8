af457fbed6a5cbd48fcaa8b98faaae6094b5d10a
visual <visual@LAB24-05>
2021-12-07 15:32:51 +0300

    Добавьте файлы проекта.

---
+++ Practtt8/Practtt8/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Practtt8
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Image image = new Image();
+            image.Source = "mqdefault.jpg";
+        }
+
+        private void dataBirth_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            int ag = DateTime.Now.Year - dataBirth.Date.Year;
+            if (DateTime.Now.Month < dataBirth.Date.Month || (DateTime.Now.Month == dataBirth.Date.Month &&
+                DateTime.Now.Month < dataBirth.Date.Month))
+            {
+                ag--;
+            }
+            age.Text = "Возраст-" + ag.ToString();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            EntryCell entry = new EntryCell();
+            entry.Placeholder = "";
+            entry.Keyboard = Keyboard.Numeric;
+            phone.Add(entry);
+        }
+    }
+}
